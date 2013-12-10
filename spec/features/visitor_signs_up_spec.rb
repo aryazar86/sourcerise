147275c1426a260ab947123a8f42c8b2287e8c1f
MatthewTLackey <lackey.matt@gmail.com>
2013-12-09 23:11:38 -0500

    Adds interaction testing infrastructure

---
+++ spec/features/visitor_signs_up_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+
+# #this example comes from a ThoughtBot blog page
+# feature 'Visitor signs up' do
+#   scenario 'with valid email and password' do
+#     sign_up_with 'valid@example.com', 'password'
+
+#     expect(page).to have_content('Sign out')
+#   end
+
+#   scenario 'with invalid email' do
+#     sign_up_with 'invalid_email', 'password'
+
+#     expect(page).to have_content('Sign in')
+#   end
+
+#   scenario 'with blank password' do
+#     sign_up_with 'valid@example.com', ''
+
+#     expect(page).to have_content('Sign in')
+#   end
+
+#   def sign_up_with(email, password)
+#     visit sign_up_path
+#     fill_in 'Email', with: email
+#     fill_in 'Password', with: password
+#     click_button 'Sign up'
+#   end
+# end
+
+#This example comes fron the capybara Github page
+describe "the signin process", :type => :feature do
+  before :each do
+    FactoryGirl.build(:user, :email => 'user@example.com', :password => 'caplin')
+  end
+
+  it "signs me in" do
+    visit '/sessions/new'
+    within("#session") do
+      fill_in 'Login', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+    end
+    click_link 'Sign in'
+    expect(page).to have_content 'Success'
+  end
+end
