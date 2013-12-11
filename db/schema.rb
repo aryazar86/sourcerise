fd4cbf843b4ff814b58452671cfdd5c3535e9244
Arya Iranpour <arya.iranpour@gmail.com>
2013-12-11 11:44:19 -0500

    Added foundation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209230247) do
+ActiveRecord::Schema.define(version: 20131209234832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "callouts", force: true do |t|
+    t.string   "subject"
+    t.datetime "deadline"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "user_roles", force: true do |t|
     t.string   "role"
