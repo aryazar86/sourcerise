49d8b1453466b9d825d0d8ea1f6dd3f6c8a8ff59
Arya Iranpour <arya.iranpour@gmail.com>
2013-12-20 14:11:58 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
- # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 UserRole.create!(
   :role => "Media"
@@ -37,45 +30,51 @@ UserRole.create!(
 #   :description => "This is the fourth sample callout description",
 #   :user_id => 1
 #   )
+issue = [["Human Rights", 0], ["Enviroment", 0], ["Poverty", 0], ["Education", 0], ["Health", 0], ["Women", 0], ["Microfinance", 0], ["Disability Inclusion", 0], ["Refugees", 0]]
 
-  location = [
-    ["North America", 0],
-     ["South America", 0], 
-     ["Africa", 0], 
-     ["Europe", 0], 
-     ["Asia", 0], 
-     ["Australia", 0], 
-     ["Canada", 1], 
-     ["USA", 1], 
-     ["Mexico", 1], 
-     ["Central America", 2], 
-     ["Latin America", 2], 
-     ["Subsaharan", 3], 
-     ["East Africa", 3], 
-     ["West Africa", 3], 
-     ["Central Africa", 3], 
-     ["Western Europe", 4], 
-     ["Eastern Europe", 4], 
-     ["Russia", 4], 
-     ["Middle East", 5], 
-     ["India", 5], 
-     ["Central Asia", 5], 
-     ["China", 5], 
-     ["Southeast Asia", 5], 
-     ["Micronesia", 6], 
-     ["New Zealand", 6]
+location = [
+   ["North America", 0],
+   ["South America", 0], 
+   ["Africa", 0], 
+   ["Europe", 0], 
+   ["Asia", 0], 
+   ["Oceania", 0], 
+   ["Canada", 1], 
+   ["USA", 1], 
+   ["Mexico", 1], 
+   ["Central America", 2],
+   ["Brazil", 2],
+   ["Hispanic America", 2]
+   ["Northern Africa", 3], 
+   ["Western Africa", 3], 
+   ["Eastern Africa", 3], 
+   ["Central Africa", 3], 
+   ["Southern Africa", 3], 
+   ["Western Europe", 4], 
+   ["Eastern Europe", 4], 
+   ["Russia", 4], 
+   ["British Isles", 4],
+   ["Scandinavia", 4],
+   ["Southern Europe", 4],
+   ["Middle East", 5], 
+   ["South Asia", 5], 
+   ["India", 5],
+   ["Central Asia", 5], 
+   ["China", 5], 
+   ["Southeast Asia", 5], 
+   ["Micronesia", 6], 
+   ["Australia and New Zealand", 6]
 ]
 
+medium = [["Article", 0], ["Quote", 0], ["Video", 0], ["TV Appearance", 0], ["Radio Appearance", 0], ["Photo", 0], ["Travel to the Field", 0]]
 
-  location.each do |l|
-    Interest.create!(
-      :name => l[0],
-      :parent_id => l[1],
-      :topic => "Location"
-      )
-  end
-
-issue = [["Human Rights", 0], ["Enviroment", 0], ["Poverty", 0]]
+location.each do |l|
+  Interest.create!(
+    :name => l[0],
+    :parent_id => l[1],
+    :topic => "Location"
+    )
+end
 
 issue.each do |i|
   Interest.create!(
@@ -85,95 +84,104 @@ issue.each do |i|
     )
 end
 
-#This should create a group of users who are interested in everything 'American'
-names = ["John", "Jack", "Phil", "Caleb", "Graham"]
-americans = []
-names.each do |n|
-  User.create!(
-    :email => "#{n}@#{n}.com",
-    :first_name => "#{n}",
-    :last_name => "#{n}",
-    :password => "pass",
-    :password_confirmation => "pass",
-    :user_role_id => 1
+medium.each do |i|
+  Interest.create!(
+    :name => i[0],
+    :parent_id => i[1],
+    :topic => "Medium"
     )
-  americans << User.last
-end
-
-all_interests = Interest.all
-
-american_interests = all_interests.select{|i| i.name.include?("America")}
-
-americans.each do |u|
-  u.interests = american_interests
-end
-
-#This creates a group of users who are interested in everything 'African'
-
-african_names = ["Jane", "Jill", "Hillary"]
-africans = []
-
-african_names.each do |n|
-  User.create!(
-    :email => "#{n}@#{n}.com",
-    :first_name => "#{n}",
-    :last_name => "#{n}",
-    :password => "pass",
-    :password_confirmation => "pass",
-    :user_role_id => 1
-    )
-  africans << User.last
-end
-
-african_interests = all_interests.select{|i| i.name.include?("Africa")}
-
-africans.each do |u|
-  u.interests = african_interests
 end
 
 
-all_interests = Interest.all
+# #This should create a group of users who are interested in everything 'American'
+# names = ["John", "Jack", "Phil", "Caleb", "Graham"]
+# americans = []
+# names.each do |n|
+#   User.create!(
+#     :email => "#{n}@#{n}.com",
+#     :first_name => "#{n}",
+#     :last_name => "#{n}",
+#     :password => "pass",
+#     :password_confirmation => "pass",
+#     :user_role_id => 1
+#     )
+#   americans << User.last
+# end
 
-american_callouts = []
-american_topics = ["Inequality", "Development", "Democracy", "Transparency", "Socialism"]
-iterating_id = 1
-american_topics.each do |c|
-  Callout.create!(
-    :subject => "#{c} in the hemisphere",
-    :deadline => "2013-12-14 02:23:00",
-    :description => "#{c} and its effects on countries on both continents.",
-    :user_id => iterating_id
-    )
-  iterating_id += 1
-  american_callouts << Callout.last
-end
+# all_interests = Interest.all
 
-american_interests = all_interests.select{|i| i.name.include?("America")}
+# american_interests = all_interests.select{|i| i.name.include?("America")}
 
-american_callouts.each do |c|
-  c.interests << american_interests
-end
+# americans.each do |u|
+#   u.interests = american_interests
+# end
+
+# #This creates a group of users who are interested in everything 'African'
+
+# african_names = ["Jane", "Jill", "Hillary"]
+# africans = []
+
+# african_names.each do |n|
+#   User.create!(
+#     :email => "#{n}@#{n}.com",
+#     :first_name => "#{n}",
+#     :last_name => "#{n}",
+#     :password => "pass",
+#     :password_confirmation => "pass",
+#     :user_role_id => 1
+#     )
+#   africans << User.last
+# end
+
+# african_interests = all_interests.select{|i| i.name.include?("Africa")}
+
+# africans.each do |u|
+#   u.interests = african_interests
+# end
 
 
-african_topics = ["Inequality", "Development", "Democracy"]
-african_callouts = []
-iterating_id = 6
-african_topics.each do |c|
-  Callout.create!(
-    :subject => "#{c} in Africa",
-    :deadline => "2013-12-14 02:23:00",
-    :description => "#{c} and its effects on countries on both continents.",
-    :user_id => iterating_id
-    )
-  iterating_id += 1
-  african_callouts << Callout.last
-end
+# all_interests = Interest.all
 
-african_interests = all_interests.select{|i| i.name.include?("Africa")}
+# american_callouts = []
+# american_topics = ["Inequality", "Development", "Democracy", "Transparency", "Socialism"]
+# iterating_id = 1
+# american_topics.each do |c|
+#   Callout.create!(
+#     :subject => "#{c} in the hemisphere",
+#     :deadline => "2013-12-14 02:23:00",
+#     :description => "#{c} and its effects on countries on both continents.",
+#     :user_id => iterating_id
+#     )
+#   iterating_id += 1
+#   american_callouts << Callout.last
+# end
 
-african_callouts.each do |c|
-  c.interests << african_interests
-end
+# american_interests = all_interests.select{|i| i.name.include?("America")}
+
+# american_callouts.each do |c|
+#   c.interests << american_interests
+# end
+
+
+# african_topics = ["Inequality", "Development", "Democracy"]
+# african_callouts = []
+# iterating_id = 6
+# african_topics.each do |c|
+#   Callout.create!(
+#     :subject => "#{c} in Africa",
+#     :deadline => "2013-12-14 02:23:00",
+#     :description => "#{c} and its effects on countries on both continents.",
+#     :user_id => iterating_id
+#     )
+#   iterating_id += 1
+#   african_callouts << Callout.last
+# end
+
+# african_interests = all_interests.select{|i| i.name.include?("Africa")}
+
+# african_callouts.each do |c|
+#   c.interests << african_interests
+# end
 
 
 
